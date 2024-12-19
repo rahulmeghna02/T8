0e0dbeced9815c6a5d421fad53a4dfcb49341125
rahulmeghna02 <coctopper08@gmail.com>
2024-12-19 12:06:00 +0530

    Add files via upload

---
+++ sharpx.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SharpX72
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7634723065:AAFYmYLjPMghtL5Yau7skGUT0wOJRnXv5mM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAHUL_DDOS_B")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1661744209")
+
+
+
+
+
+  
